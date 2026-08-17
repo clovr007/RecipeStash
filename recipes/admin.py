6e131d6de03cf3a5e9b034841c0ee1a6d7e9f7a6
Bhagya Sreenivas <avbhagya.8b@gmail.com>
2026-08-17 14:02:16 +0530

    Add Recipe model and database

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Recipe
+# username : clover
+# pass : 5678
+
+admin.site.register(Recipe)
+
 # Register your models here.
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Recipe(models.Model):
+
+    title = models.CharField(max_length=200)
+
+    source_url = models.URLField()
+
+    ingredients_list = models.TextField()
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="recipes"
+    )
+
+    def __str__(self):
+        return self.title
